c2d89f0f5518890cf3fcb620212d72672bbea7a6
WeidiDeng <dvdperfect0916@gmail.com>
2018-11-25 15:03:48 +0800

    Revert "fix bangumi next p"
    This reverts commit d157d59748b2abe468eba4f5f22e98c45a0c295e.

--- bilibili_adjustPlayer_old.user.js
+++ bilibili_adjustPlayer_old.user.js
@@ -12,7 +12,7 @@
 // @include     http*://bangumi.bilibili.com/movie/*
 // @exclude     http*://bangumi.bilibili.com/movie/
 // @description 调整B站播放器设置，增加一些实用的功能。
-// @version     1.53.2
+// @version     1.53.1
 // @grant       GM.setValue
 // @grant       GM_setValue
 // @grant       GM.getValue
@@ -2017,33 +2017,28 @@
 					var targetNode = records[i].target;
 					if (targetNode !== null) {
 						var isReload = false;
-						var reloadTimer = null;
-						clearTimeout(this.reloadTimer);
-						this.reloadTimer = window.setTimeout(function() {
-							if (isReload === false) {
-								var newPlistId, oldPListId;
-								newPlistId = reloadPList.getPListId(targetNode.baseURI);
-								oldPListId = window.adjustPlayerCurrentPListId;
-								if (newPlistId !== oldPListId) {
-									console.log('reloadPList:\nnewPlistId:' + newPlistId + "\noldPListId:" + oldPListId);
-									isReload = true;
-									observer.disconnect();
-									if (typeof GM_getValue === 'function') {
-										var setting = config.read();
-										adjustPlayer.reload(setting);
-									} else {
-										var setting = config.read();
-										setting.then(function(value) {
-											adjustPlayer.reload(value);
-										});
-									}
+						if (isReload === false) {
+							var newPlistId, oldPListId;
+							newPlistId = reloadPList.getPListId(targetNode.baseURI);
+							oldPListId = window.adjustPlayerCurrentPListId;
+							if (newPlistId !== oldPListId) {
+								console.log('reloadPList:\nnewPlistId:' + newPlistId + "\noldPListId:" + oldPListId);
+								isReload = true;
+								observer.disconnect();
+								if (typeof GM_getValue === 'function') {
+									var setting = config.read();
+									adjustPlayer.reload(setting);
 								} else {
-									reloadPList.getScreenMode();
+									var setting = config.read();
+									setting.then(function(value) {
+										adjustPlayer.reload(value);
+									});
 								}
+								break;
 							} else {
-								return;
+								reloadPList.getScreenMode();
 							}
-						}, 200);
+						}
 					}
 				}
 			});
